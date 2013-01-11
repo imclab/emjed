64963415986f4f58da74bd3e83023e9d93181a21
Yoshinori Kohyama <yykohyama@gmail.com>
2013-01-11 19:41:32 +0900

    Change clojure version from 1.3 to 1.4

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject emjed "0.4"
   :description "A program which manages programs for embedded computers"
   :dev-dependencies [[org.clojure/algo.monads "0.1.0"]]
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/core.incubator "0.1.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/core.incubator "0.1.2"]
                  [org.clojure/tools.logging "0.2.3"]
                  [server-socket "1.0.0"]
                  [overtone/at-at "1.0.0"]
-                 [cheshire "4.0.1"]]
+                 [cheshire "5.0.1"]]
   :aot [clojure.core.incubator
         clojure.tools.logging
         clojure.tools.logging.impl
