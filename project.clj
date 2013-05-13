8771f202eae70b49ca7b34f6e5434ba0f81f1ae5
Yoshinori Kohyama <yykohyama@gmail.com>
2013-05-13 13:53:17 +0900

    Release 1.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.kohyama/emjed "1.0.0"
+(defproject org.clojars.kohyama/emjed "1.0.1"
   :description "A program which manages programs for embedded computers"
   :url "https://github.com/kohyama/emjed"
   :dev-dependencies [[org.clojure/algo.monads "0.1.0"]]
