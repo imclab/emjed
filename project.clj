ae1a62826b44ca82c21019647de16939ab84224f
Yoshinori Kohyama <yykohyama@gmail.com>
2013-05-15 18:03:06 +0900

    Update versions of underlying libraries

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject org.clojars.kohyama/emjed "1.0.1"
   :description "A program which manages programs for embedded computers"
   :url "https://github.com/kohyama/emjed"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.incubator "0.1.2"]
-                 [org.clojure/tools.logging "0.2.3"]
+                 [org.clojure/tools.logging "0.2.6"]
                  [server-socket "1.0.0"]
-                 [overtone/at-at "1.0.0"]
+                 [overtone/at-at "1.1.1"]
                  [org.clojure/data.json "0.2.2"]]
   :aot :all
   :omit-source true
